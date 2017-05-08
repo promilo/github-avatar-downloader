3ee1043f5551e76382dde86d57e654fc55466341
promilo <milind.shah@mail.utoronto.ca>
2017-05-08 20:41:34 +0000

    Made requestURL variable and consoled it out

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,15 @@
 var request = require('request');
+var GITHUB_USER = "promilo";
+var GITHUB_TOKEN = "";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
